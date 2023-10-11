d158cc6fe1a310c819e4f0c79d14ec7859958460
anhthungn <124038599+anhthungn@users.noreply.github.com>
2023-10-11 23:51:05 +0200

    Add files via upload

---
+++ others/test.py
@@ -0,0 +1,31 @@
+#úkol 1
+ja = 18
+ona= 20
+if ja == ona:
+    print("Jsme stejně staří.")
+elif ja < ona:
+    print("Jsem mladší")
+else:
+    print("Ona je mladší")
+
+#úkol 2
+x = 10
+print("Umocněné na druhou se rovná",x**2)
+print("Převrácená hodnota se rovná", 1/x)
+
+
+#úkol 3
+def (obliba):
+    for i in range(1, 10)
+    if i<7:
+        print("Mám rád číslo", i)
+    else:
+        print("Číslo", i "se mi nelíbí")
+
+print(obliba)
+
+#úkol 4
+for pole in range(1, 10):
+    soucet: len(pole)
+
+print(soucet)
