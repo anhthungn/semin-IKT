1f2986f6ec8e9202f6f78b34a37488396d3bfab7
anhthungn <124038599+anhthungn@users.noreply.github.com>
2024-02-21 09:37:06 +0100

    Update game_objects.py

--- game/src/game_objects.py
+++ game/src/game_objects.py
@@ -1,11 +1,35 @@
 class Rocket:
-    def __init__(self, size, speed, smer, position, hp):
+    def __init__(self, size, speed, direction, position, hp):
         self._size = size
         self._speed = speed
-        self._smer = smer
+        self._direction = direction
         self._position = position
         self._max_hp = hp
         self._current_hp = hp
+        self._skore = 0
+    
+    def return_skore(self):
+        return self._skore
+
+    def pohyb():
+        pass
+        # nastaveni klavesy, detekce kolize: if isinstance(objekt, Bonuses)
+
+    def vykresli():
+        pass    
+        # 
+
+    def navyseni_skore():
+        pass
+    
+    def ubirani_zivotu():
+        pass
+
+    def detekce_kolize():
+        pass
+
+    def end_game():
+        pass
 
 class Bonuses:
     def __init__(self, size, position, hodnota):
@@ -13,9 +37,25 @@ class Bonuses:
         self._position = position
         self._hodnota = hodnota
 
+    def vyskresli():
+        pass
+
+class Coin(Bonuses):
+    def __init__(self, size, position, hodnota):
+        super().__init__(size, position, hodnota)
+
 class Asteroids:
-    def __init__(self, size, speed, smer, position):
+    def __init__(self, size, speed, direction, position):
         self._size = size
         self._speed = speed
-        self._smer = smer
+        self._direction = direction
         self._position = position
+
+    def pohyb():
+        pass
+        # pohyb skrze cos, sin
+    
+    def vykresli():
+        pass
+
+    
